a362abcf509bcc3dfc5ba41691b840c24dba7962
Mayck Coelho <mayckcoelho@MacBook-Air-de-Mayck.local>
2020-12-15 17:37:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# wp5-mf-react-typescript
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => <div>Olá, eu sou o React com Webpack 5.</div>;
+
+ReactDOM.render(<App />, document.getElementById("app"));
